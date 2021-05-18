6732965e652ced2f6545b411b27ac9a7616f7055
Sam Heath <sam.heath@ftpsolutions.com.au>
2021-05-18 13:36:32 +0800

    Update regex to strip non-numeric chars + more generic number validation

--- sender/sms/sms.go
+++ sender/sms/sms.go
@@ -8,11 +8,19 @@ import (
 	"regexp"
 	"strings"
 )
-var re = regexp.MustCompile(`^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$`)
+
+// https://stackoverflow.com/questions/123559/how-to-validate-phone-numbers-using-regex?page=1&tab=votes#tab-top
+var reStrip = regexp.MustCompile(`[^\+\d]+`)
+var re = regexp.MustCompile(`^(\+?\d{9,15})$`)
+
+func stripPhoneNumberCharacters(number string) string {
+	number = reStrip.ReplaceAllString(number, "")
+	return number
+}
 
 func validateSMS(job *gotell.Job) error {
 	numbers := append([]string{job.Data.To}, job.Data.CC...)
-	failedNumbers := []string{}
+	var failedNumbers []string
 	for _, s := range numbers {
 		if !re.MatchString(s) {
 			failedNumbers = append(failedNumbers, s)
@@ -25,17 +33,30 @@ func validateSMS(job *gotell.Job) error {
 	return nil
 }
 
+func transformToForJob(data gotell.JobData) (string, []string) {
+	var results []string
+	for _, number := range data.CC {
+		results = append(results, stripPhoneNumberCharacters(number))
+	}
+	return stripPhoneNumberCharacters(data.To), results
+}
+
 func MakeSMSHandler(smsSender sender.BySMS) gotell.JobHandler {
 	return func(job gotell.Job) error {
+		to, cc := transformToForJob(job.Data)
+		job.Data.To = to
+		job.Data.CC = cc
+		fmt.Println(job.Data.To)
 		err := validateSMS(&job)
+
 		if err != nil {
 			return err
 		}
 
 		smsSender.From(job.Data.From)
 		smsSender.WithBody(job.Data.Body)
-		smsSender.To(job.Data.To, job.Data.CC...)
 
+		smsSender.To(job.Data.To, job.Data.CC...)
 		return smsSender.Send()
 	}
 }
--- sender/sms/sms_test.go
+++ sender/sms/sms_test.go
@@ -9,9 +9,14 @@ import (
 )
 
 const (
-	australianMobileNumber                         = "0412345678"
+	australianMobileNumber                         = "04 12345678"
 	australianMobileNumberWithCountryCode          = "+61412345678"
 	australianMobileNumberWithCountryCodeAlternate = "0061412345678"
+	americanMobileNumber                           = "+1 (123) 456–7890"
+	britishMobileNumber                            = "07890 123456"
+	britishMobileNumberWithCountryCode             = "+447890123456"
+	thaiMobileNumber                               = "+66 2-123-4567"
+	czechMobileNumber                              = "+420 2 / 12 34 56 78"
 )
 
 func TestValidateSms(t *testing.T) {
@@ -20,14 +25,41 @@ func TestValidateSms(t *testing.T) {
 		Data: gotell.JobData{
 			To: australianMobileNumber,
 			CC: []string{
-				australianMobileNumber,
 				australianMobileNumberWithCountryCode,
 				australianMobileNumberWithCountryCodeAlternate,
+				americanMobileNumber,
+				britishMobileNumber,
+				britishMobileNumberWithCountryCode,
+				thaiMobileNumber,
+				czechMobileNumber,
 			},
 		},
 	}
 
+	to, cc := transformToForJob(job.Data)
+	job.Data.To = to
+	job.Data.CC = cc
 	err := validateSMS(&job)
 
 	assert.Nil(t, err)
+}
+
+func TestJobTransform(t *testing.T) {
+	data := gotell.JobData{
+		To: australianMobileNumber,
+		CC: []string{
+			australianMobileNumberWithCountryCode,
+			australianMobileNumberWithCountryCodeAlternate,
+			americanMobileNumber,
+			britishMobileNumber,
+			britishMobileNumberWithCountryCode,
+			thaiMobileNumber,
+			czechMobileNumber,
+		},
+	}
+
+	to, result := transformToForJob(data)
+
+	assert.Equal(t, to, "0412345678")
+	assert.Equal(t, result, []string{"+61412345678", "0061412345678", "+11234567890", "07890123456", "+447890123456", "+6621234567", "+420212345678"})
 }
